02d487bf7ca87d33389253d3d89d5c2f1341179e
Lars Ingebrigtsen <larsi@gnus.org>
2012-09-07 00:36:13 +0200

    Remove unneded includes.

--- woof.c
+++ woof.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <signal.h>
 #include <fcntl.h>
-#include <getopt.h>
-#include <dirent.h>
-#include <errno.h>
-#include <time.h>
-#include <unistd.h>
-#include <glib.h>
 #include <gmime/gmime.h>
 #include <ctype.h>
 
